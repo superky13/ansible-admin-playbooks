00983ce8c11fb44f4f92bc84a46f382155a7b455
ky13 <kritchie@redhat.com>
2018-08-17 13:46:42 -0400

    added 6 char spacing to beginning on converted priv key

--- random_scripts/priv_key_conversion.sh
+++ random_scripts/priv_key_conversion.sh
@@ -2,4 +2,4 @@
 
 ## This is used to place the contents of a priv key on a single line with line break chars inserted necessary for use as ssh_key_data is tower json templates
 read -e -p "Please relay your private key file to be converted to a string usable in a tower json template:  " priv_key_file
-cat "$priv_key_file" | grep -v "^-" | xargs | sed -e 's/ /\\n/g' | sed -e 's/^/-----BEGIN RSA PRIVATE KEY-----\\n/g' | sed -e 's/$/\\n-----END RSA PRIVATE KEY-----/g' | sed -e 's/^/"/g' | sed -e 's/$/"/g' | sed -e 's/^/"ssh_key_data":\ /g' > /tmp/$priv_key_file.converted
+cat "$priv_key_file" | grep -v "^-" | xargs | sed -e 's/ /\\n/g' | sed -e 's/^/-----BEGIN RSA PRIVATE KEY-----\\n/g' | sed -e 's/$/\\n-----END RSA PRIVATE KEY-----/g' | sed -e 's/^/"/g' | sed -e 's/$/"/g' | sed -e 's/^/"ssh_key_data":\ /g' | sed -e 's/^/      /g' > /tmp/$priv_key_file.converted
